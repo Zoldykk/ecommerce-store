7302b9f4df4f854b781b321b15fd2f119c19bb75
Zoldykk <49722575+Zoldykk@users.noreply.github.com>
2020-12-17 20:03:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ require("./config/passport")(passport)
 
 // Server Config
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 80
 
 // Middlewares
 app.use(express.static('views'))
